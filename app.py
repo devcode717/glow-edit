ac8b6d55211885ca9e0789bd8ef640b684d5452c
s-coder-web <65434170+s-coder-web@users.noreply.github.com>
2020-06-01 07:35:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from Tkinter import *
 import tkFileDialog
 
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation = tkFileDialog.asksaveasfilename()
+    file1 = open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+
+button = Button(root, text="Save", command=saveas)
+button.grid()
+
 root = Tk(className="glowEdit")
 
 text = Text(root)
